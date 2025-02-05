275116a40738d5a2c79d9f3c9241fc4ab40eda8d
Frontend Web Development (Junior) <157846063+Alina-Miroshkina@users.noreply.github.com>
2025-02-05 14:13:31 +0300

    Update Anagram.java

--- src/main/java/org/telran/lecture_09_using_algorithms_in_practice/practice/Anagram.java
+++ src/main/java/org/telran/lecture_09_using_algorithms_in_practice/practice/Anagram.java
@@ -5,3 +5,43 @@ package org.telran.lecture_09_using_algorithms_in_practice.practice;
 
 public class Anagram {
 }
+
+Решение:
+import java.util.Arrays;
+
+public class Anagram {
+
+    // Метод для проверки, являются ли две строки анаграммами
+    public static boolean areAnagrams(String str1, String str2) {
+        // Удаляем пробелы и приводим строки к нижнему регистру
+        str1 = str1.replaceAll("\\s", "").toLowerCase();
+        str2 = str2.replaceAll("\\s", "").toLowerCase();
+
+        // Если длины строк не равны, они не могут быть анаграммами
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        // Преобразуем строки в массивы символов
+        char[] charArray1 = str1.toCharArray();
+        char[] charArray2 = str2.toCharArray();
+
+        // Сортируем массивы символов
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+
+        // Сравниваем отсортированные массивы символов
+        return Arrays.equals(charArray1, charArray2);
+    }
+
+    public static void main(String[] args) {
+        String str1 = "listen";
+        String str2 = "silent";
+
+        if (areAnagrams(str1, str2)) {
+            System.out.println(str1 + " и " + str2 + " являются анаграммами.");
+        } else {
+            System.out.println(str1 + " и " + str2 + " не являются анаграммами.");
+        }
+    }
+}
